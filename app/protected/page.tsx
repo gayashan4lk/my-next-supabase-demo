01591637b173a635de52994712e2cc802f7a025b
gayashan4lk <gayashan4lk@gmail.com>
2024-04-25 08:56:48 +0530

    remove deploy button

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,4 +1,3 @@
-import DeployButton from '@/components/DeployButton'
 import AuthButton from '@/components/AuthButton'
 import { createClient } from '@/utils/supabase/server'
 import FetchDataSteps from '@/components/tutorial/FetchDataSteps'
@@ -20,12 +19,10 @@ export default async function ProtectedPage() {
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <div className="w-full">
         <div className="py-6 font-bold bg-purple-950 text-center">
-          This is a protected page that you can only see as an authenticated
-          user
+          This is a protected page that you can only see as an authenticated user
         </div>
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-            <DeployButton />
             <AuthButton />
           </div>
         </nav>
